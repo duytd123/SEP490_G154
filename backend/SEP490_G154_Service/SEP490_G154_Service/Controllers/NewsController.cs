ca0ac9059f6f7e6f338b6e27bddaee8373668e44
MrRose <anhp4869@gmail.com>
2025-10-04 19:00:37 +0700

    News

---
+++ backend/SEP490_G154_Service/SEP490_G154_Service/Controllers/NewsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SEP490_G154_Service.DTOs.News;
+using SEP490_G154_Service.Interface;
+
+namespace SEP490_G154_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly INews _newsService;
+        public NewsController(INews newsService)
+        {
+            _newsService = newsService;
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> Search(string keyword = "Vietnam")
+        {
+            var articles = await _newsService.GetNewsAsync(keyword);
+            return Ok(articles);
+        }
+
+        [HttpPost("refresh")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult ClearCache()
+        {
+            _newsService.ClearCache();
+            return Ok(new { message = "News cache has been successfully refreshed." });
+        }
+
+        [HttpPost("filter")]
+        public async Task<IActionResult> Filter([FromBody] FilterNewsRequestDTO request)
+        {
+            var result = await _newsService.FilterNewsAsync(request);
+            return Ok(result);
+        }
+    }
+}
---
+++ backend/SEP490_G154_Service/SEP490_G154_Service/DTOs/News/NewsArticleDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SEP490_G154_Service.DTOs.News
+{
+    public class NewsArticleDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string Source { get; set; }
+        public DateTime PublishedAt { get; set; }
+    }
+}
